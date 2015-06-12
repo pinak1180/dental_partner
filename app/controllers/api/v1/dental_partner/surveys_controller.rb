4ef0ac3ff77bb92ec8466514eee7ed256310daf3
pinak1180 <pinak.0605@gmail.com>
2015-06-12 12:57:49 +0530

    survey show api params fix

--- app/controllers/api/v1/dental_partner/surveys_controller.rb
+++ app/controllers/api/v1/dental_partner/surveys_controller.rb
@@ -10,6 +10,6 @@ class Api::V1::DentalPartner::SurveysController < Api::V1::BaseController
 
   def show
     @survey = Survey.valid_feeds(@current_user).find(params[:id])
-    render json: @survey, serializer_params: { show_details: true, user_id: @current_user.id }
+    render json: @survey, show_details: true, user_id: @current_user.id 
   end
 end
--- app/serializers/survey_serializer.rb
+++ app/serializers/survey_serializer.rb
@@ -2,19 +2,16 @@ class SurveySerializer < ActiveModel::Serializer
   ## Attributes ##
   attributes :id, :title, :description, :release_date, :expiry_date, :is_completed_by_user, :questions
 
-  ## Virtual attributes ##
-  params :user_id, :show_details
-
   ## Associations ##
   has_many :questions
   has_many :responses
 
   ## Instance Methods ##
   def attributes
-    super.only(:id, :title, :description, :release_date, :exppiry_date, :is_completed_by_user) if @show_details
+    super.only(:id, :title, :description, :release_date, :exppiry_date, :is_completed_by_user) if serialization_options[:show_details]
   end
 
   def is_completed_by_user
-    object.responses.find_by(user_id: @user_id).present?
+    object.responses.find_by(user_id: serialization_options[:user_id]).present?
   end
 end
