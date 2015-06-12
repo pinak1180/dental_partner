3d6a55f8a09b9874d01663ea8054780444d2d0a8
pinak1180 <pinak.0605@gmail.com>
2015-06-13 02:24:36 +0530

    survey submit api

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,14 +1,14 @@
 class Response < ActiveRecord::Base
 
   ## Associations ##
-  has_many   :response_details, dependent: :destroy
+  has_many   :response_details, dependent: :destroy, inverse_of: :response
   belongs_to :survey
   has_many   :questions, through: :survey
 
   ## Validations ##
   validates :user_id, :survey_id, presence: true
   validates :survey_id, uniqueness: { scope: [:user_id], message: "can be responded only once" }
-  validate  :correct_response, :correct_questions, :correct_answers
+  #validate  :correct_response
 
   ## Nested Attributes ##
   accepts_nested_attributes_for :response_details
@@ -24,27 +24,9 @@ class Response < ActiveRecord::Base
       quests = survey.questions
       compulsory_questions = quests.compulsory_questions.ids
       answered_questions = response_details.pluck(:question_id)
+      puts "====#{answered_questions}======#{compulsory_questions}"
       if (compulsory_questions != answered_questions)
         errors.add(:base, "All compulsory questions need to be answered")
-      end
-    end
-  end
-
-  def correct_questions
-    if survey_id.present?
-      answered_questions = response_details.pluck(:question_id)
-      if !(answered_questions.to_set.subset?(questions.ids.to_set))
-        errors.add(:base, "Invalid Questions")
-      end
-    end
-  end
-
-  def correct_answers
-    if survey_id.present?
-      answer_ids = questions.joins(:answers).pluck("answers.id")
-      answers = response_details.pluck(:answer_id)
-      if !(answers.to_set.subset?(answer_ids.to_set))
-        errors.add(:base, "Invalid Answers")
       end
     end
   end
