aca1f5448ef778817afe0d9a0fba1fe993cfe51b
pinak1180 <pinak.0605@gmail.com>
2015-06-12 13:11:02 +0530

    survey index issue pending

--- app/controllers/api/v1/dental_partner/surveys_controller.rb
+++ app/controllers/api/v1/dental_partner/surveys_controller.rb
@@ -3,13 +3,11 @@ class Api::V1::DentalPartner::SurveysController < Api::V1::BaseController
 
   def index
     @survey  = Survey.valid_feeds(@current_user).page(@page).per(@limit)
-    @survey.user = @current_user
-    @survey.show_details = false
     render json: @survey, each_serializer: SurveySerializer
   end
 
   def show
     @survey = Survey.valid_feeds(@current_user).find(params[:id])
-    render json: @survey, show_details: true, user_id: @current_user.id 
+    render json: @survey, show_details: true, user_id: @current_user.id
   end
 end
