9835eac8b1c7ed0c9f078818a42dd35459417f31
pinak <pinak.0605@gmail.com>
2015-06-14 14:36:40 +0530

    changes

--- app/serializers/forum_serializer.rb
+++ app/serializers/forum_serializer.rb
@@ -2,7 +2,7 @@ class ForumSerializer < ActiveModel::Serializer
 
   ## Attributes ##
   attributes :id, :title, :subject, :expiry_date, :release_date,
-             :tags, :expiry_date, :comments, :total_comments
+             :tags, :expiry_date, :comments, :total_comments, :total_forums, :full_name
 
   ## Associations ##
   has_many :comments
@@ -12,5 +12,14 @@ class ForumSerializer < ActiveModel::Serializer
     object.comments.size
   end
 
-  
+  def total_forums
+    Forum.all.size
+  end
+
+  def full_name
+    User.first.full_name
+  end
+
+
+
 end
