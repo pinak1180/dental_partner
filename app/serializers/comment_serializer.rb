c5deaaea5e751512249836ef6d254009a3e500c3
pinak <pinak.0605@gmail.com>
2015-06-16 10:29:42 +0530

    minor changes

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -4,14 +4,14 @@ class CommentSerializer < ActiveModel::Serializer
   attributes :id,:comment,:created_at,:user_avatar_medium,:user_avatar_thumb,:full_name
 
   def user_avatar_medium
-    #object.try(:user).medium_image
+    object.user.medium_image if object.user.present?
   end
 
   def user_avatar_thumb
-    #object.user.thumb_image
+    object.user.thumb_image if object.user.present?
   end
 
   def full_name
-    object.user.full_name
+    object.user.full_name if object.user.present?
   end
 end
