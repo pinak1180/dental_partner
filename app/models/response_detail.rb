8973d51e1c3f18e697c31e82d72f771f010783bb
pinak <pinak.0605@gmail.com>
2015-06-21 23:55:54 +0530

    changes

--- app/models/response_detail.rb
+++ app/models/response_detail.rb
@@ -8,22 +8,5 @@ class ResponseDetail < ActiveRecord::Base
   validates :question_id, :answer_id, presence: true
   validates :question_id, uniqueness: { scope: [:response_id], message: 'question can only answered once' }
   validates_presence_of :response
-  private
 
-  def correct_response
-    if survey_id.present?
-      quests = survey.questions
-      puts quests.inspect
-      compulsory_questions = quests.compulsory_questions.ids
-      puts compulsory_questions.inspect
-      answered_questions = response_details.pluck(:question_id)
-      puts 'pinak'
-      puts answered_questions.inspect
-      puts "====#{answered_questions}======#{compulsory_questions}"
-      unless compulsory_questions.include? answered_questions
-        errors.add(:base, 'All compulsory questions need to be answered')
-      end
-    end
-  end
-  
 end
