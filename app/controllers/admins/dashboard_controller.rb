759e806336060bedac824a4cb64a2db2c63eb39e
pinak1180 <pinak.0605@gmail.com>
2015-06-22 23:19:41 +0530

    dashboard changes

--- app/controllers/admins/dashboard_controller.rb
+++ app/controllers/admins/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admins::DashboardController < AdminBaseController
   def index
+    @articles_count = News.count
+    @forums_count   = Forum.count
+    @surveys_count  = Survey.count
+    @users_count    = User.count
   end
 end
