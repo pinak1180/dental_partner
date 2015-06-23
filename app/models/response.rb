637470f0dd0a975b4bd6419b1add9fa04c1c514a
pinak <pinak.0605@gmail.com>
2015-06-23 16:21:32 +0530

    survey changes

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,18 +1,17 @@
 class Response < ActiveRecord::Base
-
   ## Associations ##
-  has_many   :response_details, dependent: :destroy, inverse_of: :response
+  has_many :response_details, dependent: :destroy, inverse_of: :response
   belongs_to :survey, inverse_of: :responses
-  has_many   :questions, through: :survey
+  has_many :questions, through: :survey
 
   ## Validations ##
   validates :user_id, :survey_id, presence: true
-  validates :survey_id, uniqueness: { scope: [:user_id], message: "can be responded only once" }
-  #validates_presence_of :response_details
-  validate  :correct_response
+  validates :survey_id, uniqueness: { scope: [:user_id], message: 'can be responded only once' }
+  # validates_presence_of :response_details
+  validate :correct_response
 
   ## Nested Attributes ##
-  accepts_nested_attributes_for :response_details,:allow_destroy => :true
+  accepts_nested_attributes_for :response_details, allow_destroy: :true
 
   ## Instance Methods ##
   def display_errors
@@ -20,6 +19,7 @@ class Response < ActiveRecord::Base
   end
 
   private
+
   def correct_response
     if survey_id.present?
       quests = survey.questions
@@ -30,8 +30,8 @@ class Response < ActiveRecord::Base
       puts answered_questions.inspect
       puts "====#{answered_questions}======#{compulsory_questions}"
       puts compulsory_questions == answered_questions
-      if compulsory_questions.present? && !(compulsory_questions == answered_questions)
-        errors.add(:base, "All compulsory questions need to be answered")
+      if compulsory_questions.present? && ((compulsory_questions & answered_questions) == compulsory_questions)
+        errors.add(:base, 'All compulsory questions need to be answered')
       end
     end
   end
