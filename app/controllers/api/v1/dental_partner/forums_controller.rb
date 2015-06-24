bb045a8dc27634f0ad4863cddefd4e7eecf08c85
pinak <pinak@complitech.net>
2015-06-24 17:22:59 +0530

    scope changes

--- app/controllers/api/v1/dental_partner/forums_controller.rb
+++ app/controllers/api/v1/dental_partner/forums_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::DentalPartner::ForumsController < Api::V1::BaseController
   before_filter :authentication_user_with_authentication_token
 
   def index
-    @forum  = ::Forum.unscoped.valid_feeds(@current_user).page(@page).per(@limit)
+    @forum  = ::Forum.valid_feeds(@current_user).page(@page).per(@limit)
     render json: @forum, each_serializer: ForumSerializer
   end
 
   def show
-    @forum = ::Forum.unscoped.valid_feeds(@current_user).find(params[:id])
+    @forum = ::Forum.valid_feeds(@current_user).find(params[:id])
     render json: @forum
   end
 end
