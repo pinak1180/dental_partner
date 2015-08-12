0e4fd2db22d5f57fbcad7349834dbd8088348962
pinak <pinak.0605@gmail.com>
2015-08-12 13:26:46 +0530

    changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,16 @@
 class UserMailer < ActionMailer::Base
-  default from: "Dental Partner <no_reply@dentalpartner.com>"
+  default from: "Maven Dental Link <noreply@mavendentallink.com.au>"
 
   def send_credentials_email(user)
     @user  = user
     @email = user.email
-    mail to: @email, subject: "Dental Partner Account Details"
+    mail to: @email, subject: "Maven Dental Link Account Details"
   end
 
   def import_status(method, invalid_records)
     @invalid_records = invalid_records
     @method          = method
     @admin           = User.admin
-    mail to: @admin.email, cc: 'drewhankin@azurigroup.com.au', bcc: 'shreya.freedom@gmail.com' ,subject: "Dental Partner | User CSV Import Status"
+    mail to: @admin.email, cc: 'drewhankin@azurigroup.com.au', bcc: 'shreya.freedom@gmail.com' ,subject: "Maven Dental Link | User CSV Import Status"
   end
 end
